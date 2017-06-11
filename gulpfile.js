1c236067ae1f149693914523e94329cda80c5612
Jackson Lynch <jacksonthatsme@gmail.com>
2017-06-11 17:19:09 -0400

    Set up gulp process to process sass and concat js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
+// Define variables
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function() {
-  // place code for your default task here
+// Register Tasks
+gulp.task('sass', function() {
+  return gulp.src('./assets/sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
 });
+
+gulp.task('js:vendor', function(){
+  return gulp.src(['./assets/javascripts/vendor/jquery.min.js', './assets/javascripts/vendor/jquery-ui.min.js'])
+  .pipe(concat('vendor.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('./assets/js/'));
+});
+
+gulp.task('js:custom', function() {
+  return gulp.src('./assets/javascripts/*.js')
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./assets/js/'));
+});
+
+gulp.task('js', ['js:vendor', 'js:custom']);
+
+gulp.task('build', ['sass','js']);
+
+gulp.task('default', ['build']);
